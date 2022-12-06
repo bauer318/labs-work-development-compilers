9476c5bb830ba531efdc910d510c659b67119788
Jacques kibamba <jacqueskibamba@gmail.com>
2022-12-07 01:46:13 +0300

    Added feature A-+0=A and A*/1 = A

--- DevCompilersLW3/ParserOpt.cs
+++ DevCompilersLW3/ParserOpt.cs
@@ -20,30 +20,14 @@ namespace DevCompilersLW3
                 {
                     GetNextToken();
                     TokenNode<Token> rigthNode = Factor();
-                    Token tokenPlus = new Token(TokenType.ADDITION_SIGN, "+");
-                    if(TokenWorker.IsConstantType(rigthNode.Value.TokenType) && TokenWorker.IsConstantType(result.Value.TokenType))
-                    {
-                        result = GetOpAdd(result, rigthNode);
-                    }
-                    else
-                    {
-                        result = new TokenNode<Token>(tokenPlus, result, rigthNode);
-                    }
+                    result = GetTokenNodeResultAdditionOperation(result, rigthNode);
                     _tokenNodes.Add(result);
                 }
                 else if (_currentToken.TokenType == TokenType.SOUSTRACTION_SIGN)
                 {
                     GetNextToken();
                     TokenNode<Token> rigthNode = Factor();
-                    Token tokenMinus = new Token(TokenType.SOUSTRACTION_SIGN, "-");
-                    if (TokenWorker.IsConstantType(rigthNode.Value.TokenType) && TokenWorker.IsConstantType(result.Value.TokenType))
-                    {
-                        result = GetOpSub(result, rigthNode);
-                    }
-                    else
-                    {
-                        result = new TokenNode<Token>(tokenMinus, result, rigthNode);
-                    }
+                    result = GetTokenNodeResultSubtractionOperation(result, rigthNode);
                     _tokenNodes.Add(result);
                 }
             }
@@ -59,30 +43,14 @@ namespace DevCompilersLW3
                 {
                     GetNextToken();
                     TokenNode<Token> rigthNode = Term();
-                    Token tokenMultiply = new Token(TokenType.MULTIPLICATION_SIGN, "*");
-                    if (TokenWorker.IsConstantType(rigthNode.Value.TokenType) && TokenWorker.IsConstantType(factor.Value.TokenType))
-                    {
-                        factor = GetOpMul(factor, rigthNode);
-                    }
-                    else
-                    {
-                        factor = new TokenNode<Token>(tokenMultiply, factor, rigthNode);
-                    }
+                    factor = GetTokenNodeResultMultiplicationOperation(factor, rigthNode);
                     _tokenNodes.Add(factor);
                 }
                 else if (_currentToken.TokenType == TokenType.DIVISION_SIGN)
                 {
                     GetNextToken();
                     TokenNode<Token> rigthNode = Term();
-                    Token tokenDivision = new Token(TokenType.DIVISION_SIGN, "/");
-                    if (TokenWorker.IsConstantType(rigthNode.Value.TokenType) && TokenWorker.IsConstantType(factor.Value.TokenType))
-                    {
-                        factor = GetOpDiv(factor, rigthNode);
-                    }
-                    else
-                    {
-                        factor = new TokenNode<Token>(tokenDivision, factor, rigthNode);
-                    }
+                    factor = GetTokenNodeResultDivisionOperation(factor, rigthNode);
                     _tokenNodes.Add(factor);
                 }
             }
@@ -92,76 +60,147 @@ namespace DevCompilersLW3
         {
             return tokenNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT;
         }
-        private TokenNode<Token> GetOpAdd(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
+        private bool IsNeutralElementSubAdd(TokenNode<Token> tokenNode)
+        {
+            string lexeme = tokenNode.Value.Lexeme;
+            return lexeme.Equals("0") || lexeme.Equals("0.0");
+        }
+        private bool IsNeutralElementMulDiv(TokenNode<Token> tokenNode)
+        {
+            string lexeme = tokenNode.Value.Lexeme;
+            return lexeme.Equals("1") || lexeme.Equals("1.0");
+        }
+        
+        private TokenNode<Token> GetTokenNodeResultAdditionOperation(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
         {
             Token leafToken = null;
             TokenNode<Token> result = null;
-            if (leftNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT ||
-                rightNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT)
+            if(TokenWorker.IsConstantType(leftNode.Value.TokenType) && TokenWorker.IsConstantType(rightNode.Value.TokenType))
             {
-                double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
-                    + Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
-                string lexeme = resultValue.ToString().Replace(',', '.');
-                leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsDecimalTokenConstant(leftNode) || IsDecimalTokenConstant(rightNode))
+                {
+                    double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
+                        + Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
+                    string lexeme = resultValue.ToString().Replace(',', '.');
+                    leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
+                else
+                {
+                    int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) +
+                        Convert.ToInt32(rightNode.Value.Lexeme);
+                    string lexeme = resultValue.ToString();
+                    leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
             }
             else
             {
-                int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) +
-                    Convert.ToInt32(rightNode.Value.Lexeme);
-                string lexeme = resultValue.ToString();
-                leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsNeutralElementSubAdd(leftNode))
+                {
+                    result = rightNode;
+                }
+                else if (IsNeutralElementSubAdd(rightNode))
+                {
+                    result = leftNode;
+                }
+                else
+                {
+                    Token tokenPlus = new Token(TokenType.ADDITION_SIGN, "+");
+                    result = new TokenNode<Token>(tokenPlus, leftNode, rightNode);
+                }
+                
             }
+           
             return result;
         }
-        private TokenNode<Token> GetOpSub(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
+        private TokenNode<Token> GetTokenNodeResultSubtractionOperation(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
         {
             Token leafToken = null;
             TokenNode<Token> result = null;
-            if (leftNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT ||
-                rightNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT)
+            if (TokenWorker.IsConstantType(leftNode.Value.TokenType) && TokenWorker.IsConstantType(rightNode.Value.TokenType))
             {
-                double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
-                    - Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
-                string lexeme = resultValue.ToString().Replace(',', '.');
-                leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsDecimalTokenConstant(leftNode) || IsDecimalTokenConstant(rightNode))
+                {
+                    double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
+                        - Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
+                    string lexeme = resultValue.ToString().Replace(',', '.');
+                    leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
+                else
+                {
+                    int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) -
+                        Convert.ToInt32(rightNode.Value.Lexeme);
+                    string lexeme = resultValue.ToString();
+                    leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
             }
             else
             {
-                int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) -
-                    Convert.ToInt32(rightNode.Value.Lexeme);
-                string lexeme = resultValue.ToString();
-                leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsNeutralElementSubAdd(leftNode))
+                {
+                    result = rightNode;
+                }
+                else if (IsNeutralElementSubAdd(rightNode))
+                {
+                    result = leftNode;
+                }
+                else
+                {
+                    Token tokenMinus = new Token(TokenType.ADDITION_SIGN, "-");
+                    result = new TokenNode<Token>(tokenMinus, leftNode, rightNode);
+                }
+                
             }
+            
             return result;
         }
-        private TokenNode<Token> GetOpMul(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
+        private TokenNode<Token> GetTokenNodeResultMultiplicationOperation(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
         {
             Token leafToken = null;
             TokenNode<Token> result = null;
-            if (leftNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT ||
-                rightNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT)
+            if (TokenWorker.IsConstantType(leftNode.Value.TokenType) && TokenWorker.IsConstantType(rightNode.Value.TokenType))
             {
-                double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
-                    * Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
-                string lexeme = resultValue.ToString().Replace(',', '.');
-                leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsDecimalTokenConstant(leftNode) || IsDecimalTokenConstant(rightNode))
+                {
+                    double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
+                        * Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
+                    string lexeme = resultValue.ToString().Replace(',', '.');
+                    leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
+                else
+                {
+                    int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) *
+                        Convert.ToInt32(rightNode.Value.Lexeme);
+                    string lexeme = resultValue.ToString();
+                    leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
             }
             else
             {
-                int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) *
-                    Convert.ToInt32(rightNode.Value.Lexeme);
-                string lexeme = resultValue.ToString();
-                leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsNeutralElementMulDiv(leftNode))
+                {
+                    result = rightNode;
+                }
+                else if (IsNeutralElementMulDiv(rightNode))
+                {
+                    result = leftNode;
+                }
+                else
+                {
+                    Token tokenMul = new Token(TokenType.ADDITION_SIGN, "*");
+                    result = new TokenNode<Token>(tokenMul, leftNode, rightNode);
+                }
+                
             }
+           
             return result;
         }
-        private TokenNode<Token> GetOpDiv(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
+        private TokenNode<Token> GetTokenNodeResultDivisionOperation(TokenNode<Token> leftNode, TokenNode<Token> rightNode)
         {
             Token leafToken = null;
             TokenNode<Token> result = null;
@@ -170,23 +209,40 @@ namespace DevCompilersLW3
                 throw new ArithmeticException("Division by zero");
                 
             }
-            if (leftNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT ||
-                rightNode.Value.TokenType == TokenType.CORRECT_DECIMAL_CONSTANT)
+            if (TokenWorker.IsConstantType(leftNode.Value.TokenType) && TokenWorker.IsConstantType(rightNode.Value.TokenType))
             {
-                double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
-                    / Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
-                string lexeme = resultValue.ToString().Replace(',', '.');
-                leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsDecimalTokenConstant(leftNode) || IsDecimalTokenConstant(rightNode))
+                {
+                    double resultValue = Convert.ToDouble(leftNode.Value.Lexeme.Replace('.', ','))
+                        / Convert.ToDouble(rightNode.Value.Lexeme.Replace('.', ','));
+                    string lexeme = resultValue.ToString().Replace(',', '.');
+                    leafToken = new Token(TokenType.CORRECT_DECIMAL_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
+                else
+                {
+                    int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) /
+                        Convert.ToInt32(rightNode.Value.Lexeme);
+                    string lexeme = resultValue.ToString();
+                    leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
+                    result = new TokenNode<Token>(leafToken);
+                }
             }
             else
             {
-                int resultValue = Convert.ToInt32(leftNode.Value.Lexeme) /
-                    Convert.ToInt32(rightNode.Value.Lexeme);
-                string lexeme = resultValue.ToString();
-                leafToken = new Token(TokenType.INTEGER_CONSTANT, lexeme);
-                result = new TokenNode<Token>(leafToken);
+                if (IsNeutralElementMulDiv(rightNode))
+                {
+                    Console.WriteLine("Is neutral div " + leftNode.Value.Lexeme + " and " + rightNode.Value.Lexeme);
+                    result = leftNode;
+                }
+                else
+                {
+                    Token tokenDiv = new Token(TokenType.ADDITION_SIGN, "/");
+                    result = new TokenNode<Token>(tokenDiv, leftNode, rightNode);
+                }
+                
             }
+            
             return result;
         }
     }
